e754a6f0b5d573182bc1ac7ab0784af9e8ad2b4c
kev-mwangi <kevinmwas007@gmail.com>
2025-10-23 12:45:21 +0300

    add status

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,8 @@
 const firstArg = process.argv[2];
-const secondArg = process.argv[3];
+const converted = parseInt(firstArg);
 
-console.log(firstArg + " is " + secondArg);
+if (isNaN(converted)) {
+    console.log("Not a number");
+} else {
+    console.log("My number:", converted);
+}
