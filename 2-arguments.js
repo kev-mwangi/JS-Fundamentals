aacdff10408d1384894c3f6ab4a71c81ea3f7164
kev-mwangi <kevinmwas007@gmail.com>
2025-10-23 12:35:54 +0300

    add status

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,1 +1,9 @@
-console.log("No argument");
+const numArgs = process.argv.length - 2;
+
+if (numArgs === 0) {
+    console.log("No argument");
+} else if (numArgs === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
